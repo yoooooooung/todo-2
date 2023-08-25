42841cdfb9fa9f0fb946010e2e168d3d79efcc38
jinyoooooooung <oaoa9n@gmail.com>
2023-08-25 17:54:59 +0900

    class id명 수정

--- src/App.js
+++ src/App.js
@@ -2,21 +2,36 @@ import "./App.css";
 
 function App() {
   return (
-    <div>
-      <div id="pagetitle">
+    <div> /* 모든 컨텐츠를 다 담는 큰 태그는 무조건 1개 있어야 함 */
+
+
+      <div id="header">
         <h1>Todo-list</h1>
       </div>
-      <div id="addbox">
-        <div id="titlebox">
+      
+
+      <div id="inputbox">
+
+        <div id="title">
           <h3>Title</h3>
           <input type="text" id="title" />
         </div>
-        <div id="contentsbox">
+
+        <div id="contents">
           <h3>Contents</h3>
           <input type="text" id="contents" />
         </div>
+    
         <button className="addbutton">add</button>
+
       </div>
+
+      <div id="listbox">
+        <div class="item">
+          {/* item 내부 구조 작성 */}
+        </div>
+      </div>
+
     </div>
   );
 }
