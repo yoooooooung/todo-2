311f07782a71ad3fed97f9aa9729901dd59a64c8
yoooooooung <oaoa9n@gmail.com>
2023-08-28 01:57:05 +0900

    input 입력후 추가시, 투두 생성

--- src/App.js
+++ src/App.js
@@ -4,15 +4,9 @@ import "./App.css";
 function App() {
   const [title, setTitle] = useState("");
   const [contents, setContents] = useState("");
-
-  // input태그에 텍스트가 입력되면 위state에 넣어주는 핸들러 !
-  function change_title_handler(event) {
-    setTitle(event.target.value);
-  }
-
-  function change_contents_handler(event) {
-    setContents(event.target.value);
-  }
+  const [todos, setTodos] = useState([
+    { id: 1, title: "제목", contents: "내용~~~" },
+  ]);
 
   console.log(title);
   console.log(contents);
@@ -26,22 +20,47 @@ function App() {
       <div id="input_box">
         <div id="input_title">
           <h3>Title</h3>
-          <input type="text" id="title" onChange={change_title_handler} />
+          <input
+            type="text"
+            id="title"
+            onChange={(event) => {
+              setTitle(event.target.value);
+            }}
+            value={title}
+          />
         </div>
 
         <div id="input_contents">
           <h3>Contents</h3>
-          <input type="text" id="contents" onChange={change_contents_handler} />
+          <input
+            type="text"
+            id="contents"
+            onChange={(event) => {
+              setContents(event.target.value);
+            }}
+            value={contents}
+          />
         </div>
 
-        <button className="add_button">add</button>
+        <button
+          className="add_button"
+          onClick={() => {
+            setTodos([
+              ...todos,
+              { id: todos.length + 1, title: title, contents: contents },
+            ]);
+            setTitle("");
+            setContents("");
+          }}
+        >
+          add
+        </button>
       </div>
 
       <div id="listbox">
-        <div className="item">
+        {/* <div className="item">
           <div>
             <input type="checkbox"></input>
-            {/* <span>Done</span> */}
           </div>
           <div className="todo_title">투두 제목</div>
           <div className="todo_contents">투두 내용~~~~</div>
@@ -49,43 +68,21 @@ function App() {
             <button className="delete">DELETE</button>
             <button className="cancel">CANCEL</button>
           </div>
-        </div>
-        <div className="item">
-          <div>
-            <input type="checkbox"></input>
-            {/* <span>Done</span> */}
+        </div> */}
+        {/* jsx안에서 JS문법을 쓸 때에는 중괄호 열어야 함 */}
+        {todos.map((todo) => (
+          <div className="item" key={todo.id}>
+            <div>
+              <input type="checkbox"></input>
+            </div>
+            <div className="todo_title">{todo.title}</div>
+            <div className="todo_contents">{todo.contents}</div>
+            <div className="button_box">
+              <button className="delete">DELETE</button>
+              <button className="cancel">CANCEL</button>
+            </div>
           </div>
-          <div className="todo_title">투두 제목</div>
-          <div className="todo_contents">투두 내용~~~~</div>
-          <div className="button_box">
-            <button className="delete">DELETE</button>
-            <button className="cancel">CANCEL</button>
-          </div>
-        </div>
-        <div className="item">
-          <div>
-            <input type="checkbox"></input>
-            {/* <span>Done</span> */}
-          </div>
-          <div className="todo_title">투두 제목</div>
-          <div className="todo_contents">투두 내용~~~~</div>
-          <div className="button_box">
-            <button className="delete">DELETE</button>
-            <button className="cancel">CANCEL</button>
-          </div>
-        </div>
-        <div className="item">
-          <div>
-            <input type="checkbox"></input>
-            {/* <span>Done</span> */}
-          </div>
-          <div className="todo_title">투두 제목</div>
-          <div className="todo_contents">투두 내용~~~~</div>
-          <div className="button_box">
-            <button className="delete">DELETE</button>
-            <button className="cancel">CANCEL</button>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
