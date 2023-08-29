051fdcc69852011cc9e8bf7e6fdc32f947851ae9
jinyoooooooung <oaoa9n@gmail.com>
2023-08-29 17:55:14 +0900

    done버튼 클릭시 에러 해결 필요

--- src/App.js
+++ src/App.js
@@ -9,23 +9,20 @@ function App() {
   const [todos, setTodos] = useState([
     { id: 1, title: "제목", contents: "내용~~~", isDone:false },
   ]);
-
-  function changeDoneHandler(todo) {
-    // 1. 어떤 item의 버튼을 클릭했는지 확인 => 인자로 넘겨받은 todo를 활용하여 id 값으로 확인
-    // 2. 해당 item의 isDone key의 값을 반대로 변경 (true <-> false)
-
-    const newTodos = todos.map(clickone => {
-      if (clickone.id === todo.id) {
-        return {...clickone, isDone: !todo.isDone}
-      }
-    })
-    console.log(todo.id)
-    return setTodos(newTodos)
-  }
+  console.log(todos)
+  // function changeDoneHandler(todo) {
+  //   const newTodos = todos.map(clickone => {
+  //     if (clickone.id === todo.id) {
+  //       return {...clickone, isDone: !todo.isDone}
+  //     }
+  //   })
+  //   console.log(todo.id)
+  //   return setTodos(newTodos)
+  // }
 
   // console.log => (인터넷창)개발자도구 -> 콘솔에 나타나는 내용
-  console.log(title);
-  console.log(contents);
+  // console.log(title);
+  // console.log(contents);
 
   return (
     <div>
@@ -76,39 +73,27 @@ function App() {
       <div id="listbox">
         {todos.map((todo) => (    // map 메서드를 사용해서 todos 배열에 담긴 요소 하나하나를 꺼내기(뿌려주기)
           <div className="item" key={todo.id}>
-            <div>
-              <input type="checkbox"></input>
+            <div className="checkbox">
+              <div className={todo.isDone ? "checked" : "unchecked"}></div>
+              {todo.isDone && (
+                <div>Done!</div>
+              )}
             </div>
             <div className="todo_title">{todo.title}</div>
             <div className="todo_contents">{todo.contents}</div>
             <div className="button_box">
               <button className="delete">DELETE</button>
-
+              {/* 1. 어떤 item의 버튼을 클릭했는지 확인 => 인자로 넘겨받은 todo를 활용하여 id 값으로 확인
+                  2. 해당 item의 isDone key의 값을 반대로 변경 (true <-> false) */}
               <button
                 className={todo.isDone ? "cancel" : "done"} // isDone이 true라면 className을 cancel로 준다
-                onClick={() => {
-                  // changeDoneHandler(todo)
-                  // setTodos(prev => {
-                  //   return prev.map(clickone => {
-                  //     if (clickone.id === todo.id) {
-                  //       return {...clickone, isDone: !todo.isDone}
-                  //     }
-                  //   })
-                  // })
-                  //2
-                  setTodos(todos.map(clickone => {
-                      if (clickone.id === todo.id) {
-                        return {...clickone, isDone: !todo.isDone}
+                onClick={() => {                    // 클릭하면, 
+                  setTodos(todos.map(click => {     // 기존 todos 안의 요소들을 돌아가면서 검사
+                      if (click.id === todo.id) {   // 인자로 넘겨받은 todo를 활용하여 id 값으로 확인 => 어떤 item의 버튼을 클릭했는지 확인하기 위해.
+                        return {...click, isDone: !todo.isDone} // 해당 item의 isDone key의 값을 반대로 변경
                       }
                     })
                   )
-                  // const newTodos = todos.map(clickone => {
-                  //   if (clickone.id === todo.id) {
-                  //     return {...clickone, isDone: !todo.isDone}
-                  //   }
-                  // })
-                  // console.log(todo.id)
-                  // return setTodos(newTodos)
                 }}
               >
                 {/* jsx안에서 JS문법을 쓸 때에는 중괄호 열어야 함 */}
